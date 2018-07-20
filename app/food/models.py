cf07affb796a145af0ef65272990d0b63370ae15
nadcdc <nadcdc4@gmail.com>
2018-07-20 15:10:32 +0900

    Pension 모델 추가밑 로컬에서 마이그레이션 로컬에서만 함.

--- app/food/models.py
+++ app/food/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Pension(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to= 'post', blank =True)
+    price = models.CharField(max_length=100)
+    address = models.TextField(blank=True)
+    check_in = models.CharField(max_length=100)
+    check_out = models.CharField(max_length=100)
+    room = models = models.CharField(max_length=100)
+    info = models.TextField(blank=True)
+    theme = models.CharField(max_length=100)
